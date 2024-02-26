1a6382b335f6aeeb8a7801687a09c8bb6c78c07a
blluebru <blluebru@gmail.com>
2024-02-25 21:57:41 -0300

    exercicio solo

---
+++ exerc.js
@@ -0,0 +1,18 @@
+//4. Crie uma função que receba uma string como parâmetro e retorne a mesma string com todas as letras em maiusculo. 
+//Faça uma segunda função que retorne em minusculo.
+
+function mostraNome (nome){
+    console.log(nome.toUpperCase(nome) )
+   
+    
+}
+
+
+mostraNome("Bruna Alexandre")
+
+
+function mostraNome (nome){
+    console.log(nome.toLowerCase())
+
+
+}
---
+++ funçoes.js
@@ -0,0 +1,10 @@
+//treinando funções //
+
+var valor = 15
+
+function incrementa() {
+    console.log ("estou dentro da função.")
+};
+
+incrementa()
+console.log(valor)
